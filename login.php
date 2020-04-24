cac283c52a0f615b305e5e9f7df81b4ac7cb96a4
Jeevna Prakash <jeevnafyi@gmail.com>
2020-04-24 04:03:09 -0400

    original :(

--- login.php
+++ login.php
@@ -50,12 +50,6 @@
     <title>TutorMe Login</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="static/signin.css">
-    
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css">
-
   </head>
 
   <body class="text-center">
