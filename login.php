0fdf3488e1f7af3fd152e35dbf2538778f89e3ba
James Yun <jy2gm@virginia.edu>
2020-04-23 21:58:48 -0400

    login.php

--- login.php
+++ login.php
@@ -1,3 +1,32 @@
+<?php
+    require 'connectdb.php';
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $student_id = $_POST['id'];
+      $password = $_POST['password'];
+
+      $query = "SELECT hash FROM login WHERE student_id = '$student_id'";
+      $statement = $db->prepare($query);
+      $statement->execute();
+      $results = $statement->fetchAll();
+
+      $count = count($results);
+
+      // If result matched $computingID and $password, table row must be 1 row
+      if($count == 1) {
+        $result = $results[0];
+        $hash = $result['hash'];
+        if (password_verify($password, $hash)) {
+          $_SESSION['login_user'] = $student_id;
+          header("location: homepage.php");
+        } else {
+          $pwError = "Incorrect password";
+        }
+      }else {
+        $idError = "User does not exist";
+      }
+    }
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -10,13 +39,24 @@
 
   </head>
   <body class="text-center">
-    <form class="form-signin">
+    <form class="form-signin" method="post" action="login.php">
       <img class="mb-4" src="https://conejovalleytutor.com/wp-content/uploads/2015/06/sq-011-300x300.png" alt="TutorMe" width="120" height="120">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <div class="input-group">
+        <label for="inputId" class="sr-only">Computing ID</label>
+        <input type="text" class="form-control <?php echo isset($idError) ? 'is-invalid' : ''?>" name="id" id="inputId" class="form-control" placeholder="Computing ID" aria-describedby="email" required autofocus>
+        <div class="input-group-append">
+          <span class="input-group-text" id="email">@virginia.edu</span>
+        </div>
+        <div class="invalid-feedback">
+          <?php echo $idError ?>
+        </div>
+      </div>
       <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <input type="password" class="form-control <?php echo isset($pwError) ? 'is-invalid' : ''?>" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <div class="invalid-feedback">
+        <?php echo $pwError ?>
+      </div>
       <div class="checkbox mb-3">
         <label>
           <input type="checkbox" value="remember-me"> Remember me
--- tutorMe.sql
+++ tutorMe.sql
@@ -1,6 +1,13 @@
 -- Create and populate tables
-
 -- https://docs.google.com/document/d/1GcQTdP4tdWakXyjDmUXz2QnnzqDd228vHhhRqt60NrY/edit
+
+-- login(student_id, hash, salt)
+CREATE TABLE IF NOT EXISTS login(
+    student_id VARCHAR(7) NOT NULL UNIQUE,
+    hash VARCHAR(255) NOT NULL,
+    PRIMARY KEY (student_id)
+);
+
 -- student(student_id, phone_number, first_name, last_name, venmo_id)
 CREATE TABLE IF NOT EXISTS student (
   student_id VARCHAR(7) NOT NULL DEFAULT 'abc2xyz' UNIQUE,
